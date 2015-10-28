437ecba8cbe36dd1ca275523183c70ac3f1cc76f
na57 <na57@qq.com>
2015-10-28 19:48:24 +0800

    完成wxent-auth

---
+++ test/wxent-auth-test.js
@@ -0,0 +1,27 @@
+/* global process */
+/* global it */
+/* global describe */
+
+var should = require("should");
+var urllib = require('urllib');
+
+var base = 'http://localhost:18080/auth/';
+
+describe('Wxent-auth test', function () {
+
+	it('url', function (done) {
+		this.timeout(15000);
+		urllib.request(base + 'url', {
+			type: 'GET',
+			data: {
+				redirect_uri: 'http://hello.com'
+			}
+		}, function(err, data){
+			should.not.exist(err);
+			var c = JSON.parse(data.toString());
+			c.ret.should.eql(0);
+			should.ok(c.data);
+			done();
+		});
+	});
+});
